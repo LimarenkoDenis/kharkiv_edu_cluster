42be33fa5943261f329d1a822bdf4c9dbab9d747
Egor Korsunskiy <georgykors@gmail.com>
2019-12-26 22:10:01 +0200

    Completed Tasks

---
+++ src/assets/interfaces/interface.ts
@@ -0,0 +1,10 @@
+export interface ITeam{
+    titel : string,
+    paragraph1 : string,
+    paragraph2 : string,
+    paragraph3 : string
+}
+export interface IProjects{
+    description : string,
+    
+}
