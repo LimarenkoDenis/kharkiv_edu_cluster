ff4d676683d373dbc50343bec9c75100b5ff29e0
Alex <42402960+mralexx11@users.noreply.github.com>
2019-12-27 21:57:38 +0200

    Четвёртый экран (исследование)

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,17 +1,19 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
+import {AngularFireDatabase} from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent implements OnInit {
-  @Input() content: Observable<any>;
+export class StatisticsComponent{
+  statistics: Observable<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(db: AngularFireDatabase) {
+    this.statistics = db.list('CONTENT').valueChanges().pipe(
+      map((items) => items[1])
+    );
   }
-
 }
