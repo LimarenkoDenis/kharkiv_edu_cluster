f78f0f71b1c655682a4b033ab06271e057bf353e
Alex <42402960+mralexx11@users.noreply.github.com>
2020-01-22 15:22:39 +0200

    conflicts fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { OurProjectComponent } from './OurProjects/our-project.component';
 import { JoinTeamComponent } from  './JoinTeam/join-team.component';
 import { OurPartnersComponent } from './OurPartners/our-partners.component';
